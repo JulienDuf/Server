733e1571078aacb06d1e99ea85eec1c202891003
JulienDuf <dufresne_julien@hotmail.ca>
2015-10-23 12:48:07 -0400

    Init Server

---
+++ Server.h
@@ -0,0 +1,182 @@
+#pragma once
+#include "ServerClient.h"
+#include "Info.h"
+#include <sstream>
+
+#define SERVER_NOT_FULL "OK"
+#define SERVER_FULL "FULL"
+#define SHUTDOWN_SIGNAL "/shutdown"
+
+template<class T>
+std::string toString(const T& thing) {
+	std::ostringstream stream;
+	stream << thing;
+	return stream.str();
+}
+
+class Server {
+
+	unsigned int port;
+	unsigned int maxSockets;
+	unsigned int bufferSize;
+	unsigned int maxClients;
+
+	IPaddress serverIP; 
+	TCPsocket serverSocket;
+	std::string hostName;
+
+	TCPsocket* clientSocket;
+	bool* socketIsFree;
+	char* buffer;
+
+	SDLNet_SocketSet socketSet;
+	unsigned int clientCount;
+	bool shutdownServer;
+
+	void(*activityReaction)(Server*, ClientInfo);
+
+public:
+
+	Server(unsigned int port, unsigned int bufferSize, unsigned int maxSockets, void reaction(Server*, ClientInfo), std::string hostName) {
+
+		shutdownServer = false;
+
+		this->port = port;
+		this->bufferSize = bufferSize;
+		this->maxSockets = maxSockets;
+		this->maxClients = maxSockets - 1;
+		this->activityReaction = reaction;
+
+		this->clientSocket = new TCPsocket[maxClients];
+		this->socketIsFree = new bool[maxClients];
+		this->buffer = new char[bufferSize];
+		this->hostName = hostName;
+
+		clientCount = 0;
+
+		socketSet = SDLNet_AllocSocketSet(maxSockets);
+
+		int hostResolved = SDLNet_ResolveHost(&serverIP, NULL, port);
+
+		for (unsigned int i = 0; i < maxClients; ++i) {
+			clientSocket[i] = NULL;
+			socketIsFree[i] = true;
+		}
+
+		serverSocket = SDLNet_TCP_Open(&serverIP);
+
+		SDLNet_TCP_AddSocket(socketSet, serverSocket);
+	}
+
+	~Server() {
+		
+		for (unsigned int i = 0; i < maxClients; ++i) {
+			if (!socketIsFree[i]) {
+				SDLNet_TCP_Close(clientSocket[i]);
+				socketIsFree[i] = true;
+			}
+		}
+
+		SDLNet_TCP_Close(serverSocket);
+		SDLNet_FreeSocketSet(socketSet);
+
+		delete[] clientSocket;
+		delete[] socketIsFree;
+		delete[] buffer;
+	}
+
+	void checkForConnections(){
+		
+		int numActiveSockets = SDLNet_CheckSockets(socketSet, 1);
+		int serverSocketActivity = SDLNet_SocketReady(serverSocket);
+
+		if (serverSocketActivity != 0) {
+			if (clientCount < maxClients) {
+
+				
+				int freeSpot = -99;
+				for (unsigned int i = 0; i < maxClients; ++i) {
+					if (socketIsFree[i]) {
+
+						socketIsFree[i] = false;
+						freeSpot = i;
+						break;
+					}
+				}
+
+				std::string bufferString;
+
+				clientSocket[freeSpot] = SDLNet_TCP_Accept(serverSocket);
+				SDLNet_TCP_AddSocket(socketSet, clientSocket[freeSpot]);
+				++clientCount;
+				ServerInfo info;
+				info.clientName = "New client";
+				info.message = new std::string(SERVER_NOT_FULL);
+				info.convertToString(bufferString);
+				buffer = (char*)bufferString.c_str();
+				int msgLength = strlen(buffer) + 1;
+				SDLNet_TCP_Send(clientSocket[freeSpot], (void*)buffer, msgLength);
+			}
+			else {
+
+				TCPsocket tempSock = SDLNet_TCP_Accept(serverSocket);
+
+				strcpy(buffer, SERVER_FULL);
+				int msgLength = strlen(buffer) + 1;
+				SDLNet_TCP_Send(tempSock, (void *)buffer, msgLength);
+				SDLNet_TCP_Close(tempSock);
+			}
+		} 
+	}
+
+	void checkForActivity() {
+		
+		for (unsigned int clientNumber = 0; clientNumber < maxClients; ++clientNumber) {
+
+			int clientSocketActivity = SDLNet_SocketReady(clientSocket[clientNumber]);
+
+			if (clientSocketActivity != 0) {
+
+				int receivedByteCount = SDLNet_TCP_Recv(clientSocket[clientNumber], buffer, bufferSize);
+
+				if (receivedByteCount <= 0) {
+
+					SDLNet_TCP_DelSocket(socketSet, clientSocket[clientNumber]);
+					SDLNet_TCP_Close(clientSocket[clientNumber]);
+					clientSocket[clientNumber] = NULL;
+					socketIsFree[clientNumber] = true;
+					--clientCount;
+				}
+				else 
+					activityReaction(this, ClientInfo(buffer));
+			}
+		}
+	}
+
+	void sendToClient(ServerInfo info) {
+
+		std::string str;
+		info.convertToString(str);
+		const char* message = str.c_str();
+		unsigned int msgLength = strlen(message) + 1;
+
+		for (unsigned int i = 0; i < maxClients; ++i) {
+
+			if (msgLength > 1 && !socketIsFree[i])
+				SDLNet_TCP_Send(clientSocket[i], (void*)message, msgLength);
+		}
+	}
+
+	bool getShutdownStatus() {
+		return shutdownServer;
+	}
+
+	void setShutdownStatus(bool shutdown) {
+
+		shutdownServer = shutdown;
+	}
+
+	std::string getHostName() {
+		return hostName;
+	}
+};
---
+++ ServerClient.h
@@ -0,0 +1,117 @@
+#pragma once
+#include <iostream>
+#include "Info.h"
+
+#define QUIT_SIGNAL "/quit"
+#define CONNECTION_TIMEOUT_PERIOD 5000
+#define SOCKET_SET_POLL_PERIOD 10
+#define SHUTDOWN_SIGNAL "/shutdown"
+
+class ServerClient {
+private:
+
+	unsigned int serverPort;
+	unsigned int bufferSize;
+
+	std::string serverHostname;
+	IPaddress serverIP;
+	std::string clientName;
+	TCPsocket clientSocket;
+
+	char* buffer;
+
+	SDLNet_SocketSet socketSet;
+	bool shutdownClient;
+
+	void(*newMessageReaction)(ServerInfo);
+
+public:
+
+	ServerClient(std::string serverAddress, unsigned int serverPort, unsigned int bufferSize, void reaction(ServerInfo), std::string name) {
+
+		this->shutdownClient = false;
+		this->serverHostname = serverAddress;
+		newMessageReaction = reaction;
+
+		this->serverPort = serverPort;
+		this->bufferSize = bufferSize;
+		clientName = name;
+
+		buffer = new char[bufferSize];
+		socketSet = SDLNet_AllocSocketSet(2);
+	}
+
+	~ServerClient(){
+
+		SDLNet_TCP_Close(clientSocket);
+		SDLNet_FreeSocketSet(socketSet);
+
+		delete[] buffer;
+	}
+
+	void connectToServer(){
+
+		int hostResolved = SDLNet_ResolveHost(&serverIP, serverHostname.c_str(), serverPort);
+		
+		clientSocket = SDLNet_TCP_Open(&serverIP);
+		if (clientSocket) {
+
+			SDLNet_TCP_AddSocket(socketSet, clientSocket);
+			int activeSockets = SDLNet_CheckSockets(socketSet, CONNECTION_TIMEOUT_PERIOD);
+
+			int gotServerResponse = SDLNet_SocketReady(clientSocket);
+
+			if (gotServerResponse != 0) {
+
+				int serverResponseByteCount = SDLNet_TCP_Recv(clientSocket, buffer, bufferSize);
+				newMessageReaction(ServerInfo(buffer));
+			}
+		}
+		else {
+			shutdownClient = true;
+		}
+	}
+
+	void checkForIncomingMessages(){
+
+		ServerInfo info;
+		int activeSockets = SDLNet_CheckSockets(socketSet, SOCKET_SET_POLL_PERIOD);
+
+		if (activeSockets != 0) {
+
+			int gotMessage = SDLNet_SocketReady(clientSocket);
+
+			if (gotMessage != 0) {
+				int serverResponseByteCount = SDLNet_TCP_Recv(clientSocket, buffer, bufferSize);
+
+				if (serverResponseByteCount != 0) {
+					info = ServerInfo(buffer);
+
+					if (*info.message == SHUTDOWN_SIGNAL)
+						shutdownClient = true;
+					else
+						newMessageReaction(info);
+				}
+			}
+		}
+	}
+
+	void sendToServer(ClientInfo info) {
+
+		std::string str;
+		info.convertToString(str);
+		const char* message = str.c_str();
+
+		unsigned int msgLength = strlen(message) + 1;
+
+		SDLNet_TCP_Send(clientSocket, (void *)message, msgLength);
+	}
+
+	bool getShutdownStatus() {
+		return shutdownClient;
+	}
+
+	std::string getClientName() {
+		return clientName;
+	}
+};
